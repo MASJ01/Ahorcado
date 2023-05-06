40b715cc63b31e514540842015a3db03469b79fa
Mario Arturo sierra juarez <mario_asj@outlook.com>
2023-05-06 15:11:06 -0600

    Segundo commit

---
+++ Ahorcado/Form1.Designer.cs
@@ -0,0 +1,187 @@
+﻿namespace Ahorcado
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador requerida.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén utilizando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben eliminar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido del método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbl = new System.Windows.Forms.Label();
+            this.fichas_abecedario1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.fl_palabras = new System.Windows.Forms.FlowLayoutPanel();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.BTN_INICIAR = new System.Windows.Forms.PictureBox();
+            this.pic_ahorcado = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.LBL_MENSAJE = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.TXT_VIDAS = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.BTN_INICIAR)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic_ahorcado)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lbl
+            // 
+            this.lbl.AutoSize = true;
+            this.lbl.BackColor = System.Drawing.Color.Lime;
+            this.lbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl.Location = new System.Drawing.Point(238, 85);
+            this.lbl.Name = "lbl";
+            this.lbl.Size = new System.Drawing.Size(151, 20);
+            this.lbl.TabIndex = 1;
+            this.lbl.Text = "Adivina la palabra";
+            // 
+            // fichas_abecedario1
+            // 
+            this.fichas_abecedario1.BackColor = System.Drawing.Color.Red;
+            this.fichas_abecedario1.Location = new System.Drawing.Point(93, 108);
+            this.fichas_abecedario1.Name = "fichas_abecedario1";
+            this.fichas_abecedario1.Size = new System.Drawing.Size(467, 281);
+            this.fichas_abecedario1.TabIndex = 2;
+            // 
+            // fl_palabras
+            // 
+            this.fl_palabras.BackgroundImage = global::Ahorcado.Properties.Resources.kratos;
+            this.fl_palabras.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.fl_palabras.Location = new System.Drawing.Point(144, 395);
+            this.fl_palabras.Name = "fl_palabras";
+            this.fl_palabras.Size = new System.Drawing.Size(939, 167);
+            this.fl_palabras.TabIndex = 0;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::Ahorcado.Properties.Resources.God_of_War;
+            this.pictureBox2.Location = new System.Drawing.Point(12, 349);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(126, 83);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 3;
+            this.pictureBox2.TabStop = false;
+            // 
+            // BTN_INICIAR
+            // 
+            this.BTN_INICIAR.BackgroundImage = global::Ahorcado.Properties.Resources.jugar;
+            this.BTN_INICIAR.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.BTN_INICIAR.Image = global::Ahorcado.Properties.Resources.jugar;
+            this.BTN_INICIAR.Location = new System.Drawing.Point(12, 12);
+            this.BTN_INICIAR.Name = "BTN_INICIAR";
+            this.BTN_INICIAR.Size = new System.Drawing.Size(467, 50);
+            this.BTN_INICIAR.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.BTN_INICIAR.TabIndex = 0;
+            this.BTN_INICIAR.TabStop = false;
+            this.BTN_INICIAR.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // pic_ahorcado
+            // 
+            this.pic_ahorcado.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pic_ahorcado.Location = new System.Drawing.Point(649, 108);
+            this.pic_ahorcado.Name = "pic_ahorcado";
+            this.pic_ahorcado.Size = new System.Drawing.Size(231, 190);
+            this.pic_ahorcado.TabIndex = 4;
+            this.pic_ahorcado.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Yellow;
+            this.label1.Font = new System.Drawing.Font("Microsoft Tai Le", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(485, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(121, 21);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "PUNTUACION:";
+            // 
+            // LBL_MENSAJE
+            // 
+            this.LBL_MENSAJE.AutoSize = true;
+            this.LBL_MENSAJE.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LBL_MENSAJE.Location = new System.Drawing.Point(413, 203);
+            this.LBL_MENSAJE.Name = "LBL_MENSAJE";
+            this.LBL_MENSAJE.Size = new System.Drawing.Size(372, 73);
+            this.LBL_MENSAJE.TabIndex = 6;
+            this.LBL_MENSAJE.Text = "Game Over";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Fuchsia;
+            this.label2.Font = new System.Drawing.Font("Leelawadee UI", 14.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(831, 26);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(74, 25);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "VIDAS ";
+            // 
+            // TXT_VIDAS
+            // 
+            this.TXT_VIDAS.Location = new System.Drawing.Point(911, 26);
+            this.TXT_VIDAS.Name = "TXT_VIDAS";
+            this.TXT_VIDAS.Size = new System.Drawing.Size(114, 20);
+            this.TXT_VIDAS.TabIndex = 8;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::Ahorcado.Properties.Resources.God_of_War1;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(1095, 563);
+            this.Controls.Add(this.TXT_VIDAS);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.LBL_MENSAJE);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pic_ahorcado);
+            this.Controls.Add(this.fl_palabras);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.fichas_abecedario1);
+            this.Controls.Add(this.lbl);
+            this.Controls.Add(this.BTN_INICIAR);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.BTN_INICIAR)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic_ahorcado)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox BTN_INICIAR;
+        private System.Windows.Forms.Label lbl;
+        private System.Windows.Forms.FlowLayoutPanel fichas_abecedario1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.FlowLayoutPanel fl_palabras;
+        private System.Windows.Forms.PictureBox pic_ahorcado;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label LBL_MENSAJE;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox TXT_VIDAS;
+    }
+}
+
---
+++ Ahorcado/Form1.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ahorcado
+{
+    public partial class Form1 : Form
+    {
+        char[] palabrasadivinadas;
+        char[] palabraseleccionada;
+        char[] alfabeto;
+        String[] palabras;
+        int oportunidades;
+        int puntaje = 2; 
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            iniciar_juego(); 
+        }
+
+        private void iniciar_juego()
+        {
+            fichas_abecedario1.Controls.Clear();
+            fichas_abecedario1.Enabled = true;
+            pic_ahorcado.Image = null;
+            BTN_INICIAR.Enabled = false;
+            LBL_MENSAJE.Visible = false;
+            oportunidades = 0;
+            TXT_VIDAS.Text = puntaje.ToString(); 
+            TXT_VIDAS.Enabled = false; 
+            BTN_INICIAR.Image = Properties.Resources.en_juego;
+            palabras = new string[] {"ZEUS","ARES","POSEIDON","HERMES","APOLO","HERA","ARTEMISA","HEFESTO","ATENEA","KRATOS"};
+            alfabeto = "ABCDEFGHIJKLMNÑOPQRSTUWXYZ".ToCharArray(); //Toda esta cadena es un conjunto de caracteres que se guardan el el vector alfabeto
+            Random palabra_seleccionada1 = new Random();
+            int indice_palabra_selecc = palabra_seleccionada1.Next(0,palabras.Length);
+            palabraseleccionada = palabras[indice_palabra_selecc].ToUpper().ToCharArray();
+            palabrasadivinadas = palabraseleccionada; 
+            foreach (char letra_alfabeto in alfabeto) // Coloca el abecedario
+            {
+                Button btnletra = new Button();
+                btnletra.Tag = "";
+                btnletra.Text = letra_alfabeto.ToString();
+                btnletra.Width = 60;
+                btnletra.Height = 40;
+                btnletra.ForeColor = Color.Red;
+                btnletra.Font = new Font(btnletra.Font.Name,15,FontStyle.Bold);
+                btnletra.BackColor = Color.Blue;
+                btnletra.Name = letra_alfabeto.ToString();
+                fichas_abecedario1.Controls.Add(btnletra); // Siempre va al final 
+                btnletra.Click += comparar; //Se asigna una accion
+            }
+
+            fl_palabras.Controls.Clear();
+
+            for (int indice_valor_letra = 0; indice_valor_letra < palabraseleccionada.Length; indice_valor_letra++) // Coloca la palabra que se busca
+            {
+                Button letra = new Button();
+                letra.Tag = palabraseleccionada[indice_valor_letra].ToString();
+                letra.Width = 46; //Ancho
+                letra.Height = 80; //Largo
+                letra.ForeColor = Color.BlueViolet; //Color de letra
+                letra.Text = "$"; //Simbolo
+                letra.Font = new Font(letra.Font.Name, 32, FontStyle.Bold);// Tipo de letra
+                letra.BackColor = Color.LightBlue; //Color de fondo
+                letra.FlatStyle = FlatStyle.Flat; //Es para apariencia
+                letra.Name = "ADIVINADO" + indice_valor_letra.ToString();
+                fl_palabras.Controls.Add(letra);
+            }
+        }
+
+        void comparar(object sender, EventArgs e)
+        {
+            bool encontrado = false;
+            Button btn = (Button)sender; //Se extrae el valor del boton
+            btn.BackColor = Color.LightGreen;
+            btn.ForeColor = Color.Purple;
+            btn.Enabled = false;
+
+            for (int indice_revisar = 0; indice_revisar < palabrasadivinadas.Length; indice_revisar++)
+            {
+                if (palabrasadivinadas[indice_revisar] == char.Parse(btn.Text)) // Se convierte a carcater y compara 
+                {
+                    Button tbx = this.Controls.Find("ADIVINADO" + indice_revisar,true).FirstOrDefault() as Button;
+                    tbx.Text = palabrasadivinadas[indice_revisar].ToString();
+                    palabrasadivinadas[indice_revisar] = '$';
+                    encontrado = true;
+                    
+                }
+            }
+
+            bool ganaste = true; // Se declara una variable
+
+            for (int indice_palabra_adivinada = 0; indice_palabra_adivinada < palabrasadivinadas.Length; indice_palabra_adivinada++) // Es para saber si gane
+            {
+                if (palabrasadivinadas[indice_palabra_adivinada] != '$')
+                {
+                    ganaste = false; 
+                }
+            }
+
+            if (ganaste == true)
+            {
+                MessageBox.Show("FELICIDADES ADIVINASTE LA LETRA", "ALERTA", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                puntaje++; 
+                BTN_INICIAR.Image = Properties.Resources.jugar;
+                BTN_INICIAR.Enabled = true; 
+            }
+
+            if (encontrado == false)
+            {
+                oportunidades += 1;
+                pic_ahorcado.Image = (Bitmap)Properties.Resources.ResourceManager.GetObject("ahorcado" + oportunidades);    //Se extrae la imagen del ahorcado
+            }
+
+            if (oportunidades == 10)
+            {
+                LBL_MENSAJE.Visible = true;
+                puntaje--; 
+
+                for (int indice_letras1 = 0; indice_letras1 < palabraseleccionada.Length; indice_letras1++)
+                {
+                    Button btn_letras = this.Controls.Find("ADIVINADO" + indice_letras1, true).FirstOrDefault() as Button;
+                    btn_letras.Text = btn_letras.Tag.ToString(); 
+                }
+
+                fichas_abecedario1.Enabled = false;
+                BTN_INICIAR.Image = Properties.Resources.jugar;
+                BTN_INICIAR.Enabled = true; 
+            }
+
+            if (puntaje == 0)
+            {
+                LBL_MENSAJE.Visible = true;
+                MessageBox.Show("SE TE ACABARON LAS VIDAS", "ALERTA", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                Application.Exit(); 
+            }
+        }
+    }
+    //Tarea
+} // Inicialmente tiene 2 vidas, si se adivina la palabra se suma una vida, sino se le resta
